7b51e20de1bf9d4cbdf426d9d74f8d2c287383c7
utmboffice.chen@gmail.com <utmboffice.chen@gmail.com>
2018-08-18 10:54:26 -0400

    Initial Commit

---
+++ applePayAndIAP_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  applePayAndIAP_demo
+//
+//  Created by Edwin on 2018/8/18.
+//  Copyright © 2018 Edwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
